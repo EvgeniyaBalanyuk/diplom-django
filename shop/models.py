93f45e6b53251ed385644a235fda39a5f5ced43c
Evgeniya <89214303020@mail.ru>
2024-08-14 19:40:36 +0300

    add models.py for app shop

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)  # название категории
+    slug = models.SlugField(max_length=200,   # создание индекса
+                            unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),  # индекс по полю name
+        ]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,  # внешний ключ к модели Category, взаимосвязь один ко многим, товар принадлежит к одной категории, категория содержит несколько товаров
+                                 related_name='products',
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)  # название товара
+    slug = models.SlugField(max_length=200)  # для создания URL-адресов
+    image = models.ImageField(upload_to='products/%Y/%m/%d', # изображение товара
+                              blank=True)
+    description = models.TextField(blank=True)  # описание товара
+    price = models.DecimalField(max_digits=10,  # цена товара
+                                decimal_places=2)
+    available = models.BooleanField(default=True)  # наличие или отсутствие товара
+    created = models.DateTimeField(auto_now_add=True)  # дата и время создания объекта
+    updated = models.DateTimeField(auto_now=True)  # дата и время обновления объекта
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),  # повышение производительности запросов
+            models.Index(fields=['name']),  # индекс по полю name
+            models.Index(fields=['-created']),  #  индекс по полю created определен в убывающем порядке
+        ]
+
+    def __str__(self):
+        return self.name
